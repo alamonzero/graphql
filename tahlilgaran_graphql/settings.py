d863e5ca280b8739b7bc54dc8137ef0311cf33ac
Arman Talebi <alamonzero@gmail.com>
2021-07-08 20:55:28 +0430

    adding migrations and database file for liara

--- tahlilgaran_graphql/settings.py
+++ tahlilgaran_graphql/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "tahlilgaran_graphql.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "tahlilgaran_db/db.sqlite3",
     }
 }
 
